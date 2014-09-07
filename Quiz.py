29641c3438d9fb54cb1cb741e7dbe1e8ab67c0d1
Shreya M <gymnastickid.sm@gmail.com>
2014-09-07 02:36:39 -0400

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -5,18 +5,21 @@ count = 0
  
 
 
-Q = ["What is a non metal that remains liquid at room temperature? ",
-     "Chlorophyll is a naturally occurring chelate compound in which central metal is: ",
-     "Which of the following is used in pencils? ",
-     "Which of the following metals forms an amalgam with other metals? ",
-     "The gas usually filled in the electric bulb is: ",
-     "Which of the following is used as a moderator in a nuclear reactor? ",
-     "Isotopes are separated by: ",
-     "Which radioactive pollutant has recently drawn to public, due to its occurrence in the building material? ",
-     "Who suggested that most of the mass of the atom is located in the nucleus? ",
+Q = ["What is a non metal that remains liquid at room temperature?",
+     "Chlorophyll is a naturally occurring compound. Which central metal is it? ",
+     "What is used in pencils in order to write?",
+     "Which of the following metals forms an amalgam/mixture with other metals?",
+     "The gas usually filled in the electric light bulb is ",
+     "What is used as a moderator in a nuclear reactor?",
+     "Isotopes are separated by ",
+     "What radioactive pollutant has drawn public attention, due to its occurrence in the building material?",
+     "Who suggested that most of the mass in the atom is located in the nucleus?",
      "According to Avogadro's Hypothesis, the smallest particle of an element or a compound, that can exist independently, is called a(n) ",
-     "Nuclear fission is caused by the impact of: ",
-     "How many colours the sunlight spectrum has? "]
+     "Nuclear fission is caused by the impact of ",
+     "How many colors are in the sunlight spectrum?",
+     "What color is bromine?",
+     "The hardest substance available on earth is ",
+     "The variety of coal in which the deposit contains recognizable traces of the original plant material is "]
 A = ["bromine",
      "magnesium",
      "graphite",
@@ -28,18 +31,22 @@ A = ["bromine",
      "rutherford",
      "molecule",
      "neutron",
-     "seven"]
+     "seven",
+     "red",
+     "diamond",
+     "peat"]
 
-i = randint(0, 11)
+i = randint(0, 14)
 answer = raw_input("\nQuestion 1:" + Q[i])
 
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
-    print '\033[1;31mWrong\033[1;m'
+    print "Wrong"
 
-i = randint(0, 11) 
+i = randint(0, 14) 
+
 answer = raw_input("\nQuestion 2:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -47,79 +54,85 @@ if answer.lower() == A[i]:
 else:
     print "Wrong"
 
-i = randint(0, 11)
+    i = randint(0, 14)
 answer = raw_input("\nQuestion 3:" + Q[i])
+
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
 
-i = randint(0, 11) 
+i = randint(0, 14) 
+
 answer = raw_input("\nQuestion 4:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
-    
-i = randint(0, 11)
+i = randint(0, 14)
 answer = raw_input("\nQuestion 5:" + Q[i])
+
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
 
-i = randint(0, 11) 
+i = randint(0, 14) 
+
 answer = raw_input("\nQuestion 6:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
-    
-i = randint(0, 11)
+i = randint(0, 14)
 answer = raw_input("\nQuestion 7:" + Q[i])
+
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
 
-i = randint(0, 11) 
+i = randint(0, 14) 
+
 answer = raw_input("\nQuestion 8:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
-    
-i = randint(0, 11)
+i = randint(0, 14)
 answer = raw_input("\nQuestion 9:" + Q[i])
+
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
 
-i = randint(0, 11) 
+i = randint(0, 14) 
+
 answer = raw_input("\nQuestion 10:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
-    
-i = randint(0, 11)
+i = randint(0, 14)
 answer = raw_input("\nQuestion 11:" + Q[i])
+
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
 else:
     print "Wrong"
 
-i = randint(0, 11) 
+i = randint(0, 14) 
+
 answer = raw_input("\nQuestion 12:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -128,4 +141,4 @@ else:
     print "Wrong"
 
 
-print "\nOut of 10 questions asked.  You got %d of them right." % (count)
+print "\nOut of 12 questions asked, you got %d of them right." % (count)
