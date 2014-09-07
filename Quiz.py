f3f37006e88956d7c69d948ae5331e4b9b62befd
Aditi Bhandari <aditibhandari24@gmail.com>
2014-09-07 03:56:46 -0400

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -5,23 +5,23 @@ count = 0
  
 
 
-Q = ["What is a non metal that remains liquid at room temperature?",
+Q = ["What is a non metal that remains liquid at room temperature? ",
      "Chlorophyll is a naturally occurring compound. Which central metal is it? ",
-     "What is used in pencils in order to write?",
-     "Which metal forms an amalgam/mixture with other metals?",
-     "The gas usually filled in the electric light bulb is ",
-     "What is used as a moderator in a nuclear reactor?",
-     "Isotopes are separated by ",
-     "What radioactive pollutant has drawn public attention, due to its occurrence in the building material?",
-     "Who suggested that most of the mass in the atom is located in the nucleus?",
+     "What is used in pencils in order to write? ",
+     "Which metal forms an amalgam/mixture with other metals? ",
+     "The gas usually filled in the electric light bulb is: ",
+     "What is used as a moderator in a nuclear reactor? ",
+     "Isotopes are separated by: ",
+     "What radioactive pollutant has drawn public attention, due to its occurrence in the building material? ",
+     "Who suggested that most of the mass in the atom is located in the nucleus ?",
      "According to Avogadro's Hypothesis, the smallest particle of an element or a compound, that can exist independently, is called a(n) ",
-     "Nuclear fission is caused by the impact of ",
-     "How many colors are in the sunlight spectrum?",
-     "What color is bromine?",
-     "The hardest substance available on earth is ",
+     "Nuclear fission is caused by the impact of: ",
+     "How many colors are in the sunlight spectrum? ",
+     "What color is bromine? ",
+     "The hardest substance available on earth is: ",
      "The variety of coal in which the deposit contains recognizable traces of the original plant material is ",
-     "What is the only substance that expands as it freezes?",
-     "What is the only letter that does appear on the periodic table?"]
+     "What is the only substance that expands as it freezes? ",
+     "What is the only letter that does appear on the periodic table? "]
 A = ["bromine",
      "magnesium",
      "graphite",
@@ -39,6 +39,7 @@ A = ["bromine",
      "peat",
      "water",
      "j"]
+     
 moreinfo = ["http://www.softschools.com/facts/periodic_table/bromine_facts/213/",
             "http://scifun.chem.wisc.edu/chemweek/chlrphyl/chlrphyl.html",
             "http://www.sciencekids.co.nz/sciencefacts/chemistry/carbon.html",
@@ -60,7 +61,6 @@ moreinfo = ["http://www.softschools.com/facts/periodic_table/bromine_facts/213/"
 
 i = randint(0, 14)
 answer = raw_input("Question 1:" + Q[i])
-
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
@@ -70,7 +70,6 @@ else:
 
 
 i = randint(0, 14) 
-
 answer = raw_input("\nQuestion 2:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -79,9 +78,8 @@ else:
     print "Wrong \nCorrect Answer : " + A[i]
     print "To learn more visit this site" + moreinfo[i]
 
-    i = randint(0, 14)
+i = randint(0, 14)
 answer = raw_input("\nQuestion 3:" + Q[i])
-
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
@@ -90,7 +88,6 @@ else:
     print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
-
 answer = raw_input("\nQuestion 4:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -101,7 +98,6 @@ else:
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 5:" + Q[i])
-
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
@@ -110,7 +106,6 @@ else:
     print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
-
 answer = raw_input("\nQuestion 6:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -121,7 +116,6 @@ else:
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 7:" + Q[i])
-
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
@@ -130,7 +124,6 @@ else:
     print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
-
 answer = raw_input("\nQuestion 8:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -141,7 +134,6 @@ else:
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 9:" + Q[i])
-
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
@@ -150,7 +142,6 @@ else:
     print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
-
 answer = raw_input("\nQuestion 10:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
@@ -161,7 +152,6 @@ else:
 
 i = randint(0, 14)
 answer = raw_input("\nQuestion 11:" + Q[i])
-
 if answer.lower() == A[i]:
     print "Correct"
     count = count + 1
@@ -170,7 +160,6 @@ else:
     print "To learn more visit this site" + moreinfo[i]
 
 i = randint(0, 14) 
-
 answer = raw_input("\nQuestion 12:" + Q[i])
 if answer.lower() == A[i]:
     print "Correct"
